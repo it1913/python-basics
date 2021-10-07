b248c5ba7f9930bf743d8b7bc1626fc80c094109
it1913 <66723368+it1913@users.noreply.github.com>
2021-10-07 11:29:38 +0200

    03.1-types

--- 03-types/list.py
+++ 03-types/list.py
@@ -9,6 +9,7 @@ https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PLDDhGQzLtPdbS987RIIT8WKwb-L_K2
 https://www.w3schools.com/python/python_lists.asp
 https://macek.sandbox.cz/texty/python-tutorial-cz/tut/node5.html
 '''
+import string
 
 '''
 Příklady různých seznamů v Pythonu:
@@ -285,11 +286,24 @@ print(f'\tSbalení seznamů do proměnné values: {values}\n')
 # Snažte se vždy o co nejzhuštěnější kód - ideálně na 1 řádku (+ další řádek s kontrolním výpisem proměnné)
 # import knihovny pro generování náhodných čísel
 from random import randint
-
-
-
 print(f'\n*************************************\nCvičení 2\n*************************************')
-
+#a
+hundreds= list(range(1,2001))
+hundreds = [x for x in range(200, 2001, 200)]
+print(hundreds)
+#b
+import random
+ascii = [x for i in range(50) for x in random.choice(string.ascii_uppercase)]
+print(ascii)
+#c
+hundreds = hundreds[3:-3]
+print(hundreds)
+#d
+unique = [x for x in ascii if(ascii.count(x)==1)]
+print(unique)
+#e
+zipped = zip(hundreds, unique)
+print(list(zipped))
 
 
 # ??? 3. cvičení ???
@@ -302,3 +316,16 @@ print(f'\n*************************************\nCvičení 2\n******************
 # Záznamy budou seřazeny podle věku (sestupně).
 
 print(f'\n*************************************\nCvičení 3\n*************************************')
+women = list(filter(lambda person: person[2]== "žena", persons))
+women = [person for person in persons if person[2]== "žena"]
+for person in women:
+    print(f"{person[0]}\n" + "-" * len(person[0]))
+
+ipeople = list(filter(lambda person: person[0].count("i") > 0 or person[0].count("I") > 0, persons))
+ipeople = [person for person in persons if person[0].count("i") > 0 or person[0].count("I")>0]
+ipeople.sort(key=lambda item: item[1], reverse=True)
+print(ipeople)
+ipeopleCSV = "index;jmeno;vel;pohlavi;\n"
+for i in range(len(ipeople)):
+    ipeopleCSV += f"{i};" + ";".join(map(str, ipeople[i])) + "\n"
+    print(ipeopleCSV)
